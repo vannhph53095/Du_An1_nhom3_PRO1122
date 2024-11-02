7ba8295856263af1d3b0f8136334b2c792cd6382
Nguyễn Hoàng Văn <vannhph53095@gmail.com>
2024-11-02 22:28:50 +0700

    Commit ban đầu

---
+++ app/src/test/java/fpoly/ph53095/nhom3_du_an_1_pro1122/ExampleUnitTest.java
@@ -0,0 +1,17 @@
+package fpoly.ph53095.nhom3_du_an_1_pro1122;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+/**
+ * Example local unit test, which will execute on the development machine (host).
+ *
+ * @see <a href="http://d.android.com/tools/testing">Testing documentation</a>
+ */
+public class ExampleUnitTest {
+    @Test
+    public void addition_isCorrect() {
+        assertEquals(4, 2 + 2);
+    }
+}
---
+++ settings.gradle.kts
@@ -0,0 +1,24 @@
+pluginManagement {
+    repositories {
+        google {
+            content {
+                includeGroupByRegex("com\\.android.*")
+                includeGroupByRegex("com\\.google.*")
+                includeGroupByRegex("androidx.*")
+            }
+        }
+        mavenCentral()
+        gradlePluginPortal()
+    }
+}
+dependencyResolutionManagement {
+    repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
+    repositories {
+        google()
+        mavenCentral()
+    }
+}
+
+rootProject.name = "Nhom3_Du_An_1_PRO1122"
+include(":app")
+ 
